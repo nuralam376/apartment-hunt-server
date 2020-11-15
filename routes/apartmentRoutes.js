337537204ec1cf984b3136644f41e302284347f8
Nur A Alam <nuraalam376@gmail.com>
2020-11-15 23:41:21 +0600

    Implemented apartment routes

--- routes/apartmentRoutes.js
+++ routes/apartmentRoutes.js
@@ -1,5 +1,7 @@
+const ObjectId = require("mongodb").ObjectID;
+
 module.exports = (app, apartmentCollection) => {
-  // Gets all services
+  // Gets all apartments
   app.get("/getAllHouses", (req, res) => {
     apartmentCollection
       .find({})
@@ -10,5 +12,49 @@ module.exports = (app, apartmentCollection) => {
           return res.send(documents);
         }
       });
+  });
+
+  //Adds new apartments
+  app.post("/addHouse", (req, res) => {
+    const { serviceTitle, price, location, noBedroom, noBathroom } = req.body;
+    const thumbnailFile = req.files.thumbnail;
+
+    const thumbnailImageFile = thumbnailFile.data;
+    const encodedThumbnailImage = thumbnailImageFile.toString("base64");
+
+    const thumbnailImage = {
+      contentType: thumbnailFile.mimeType,
+      size: thumbnailFile.size,
+      img: Buffer.from(encodedThumbnailImage, "base64"),
+    };
+
+    apartmentCollection
+      .insertOne({
+        serviceTitle,
+        price,
+        location,
+        noBedroom,
+        noBathroom,
+        thumbnailImage,
+      })
+      .then((result) => {
+        return res.send(result.insertedCount > 0);
+      })
+      .catch((err) => console.log(err));
+  });
+
+  // Gets the individual apartment information
+  app.get("/getAllHouses/:id", (req, res) => {
+    const id = req.params.id;
+
+    apartmentCollection
+      .findOne({ _id: ObjectId(id) })
+      .then((service) => {
+        if (service) {
+          return res.send(service);
+        }
+        return res.send(null);
+      })
+      .catch((err) => console.log(err));
   });
 };
