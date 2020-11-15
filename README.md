b591510ec7a6aac7078107f4d480573d48d683ac
Nur A Alam <nuraalam376@gmail.com>
2020-11-15 20:52:10 +0600

    Initial Server setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Apartment Hunt API
+
+## Live Website
+
+#### Technology Used -
+
+1. Node.js
+2. Express.js
+3. MongoDB
+4. Heroku
+
+#### Features -
---
+++ index.js
@@ -0,0 +1,14 @@
+// Required Modules
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
