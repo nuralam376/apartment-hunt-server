813d91831ecfcef6b095969e43183ab54369ecf2
Nur A Alam <nuraalam376@gmail.com>
2020-11-15 21:36:06 +0600

    Setup Mongodb connection

--- index.js
+++ index.js
@@ -2,13 +2,36 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Mongodb Connection setup
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${user}:${password}@cluster0.wc4e7.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// MongoDB and server starts
+client.connect((err) => {
+  if (err) {
+    console.log("Database error", err);
+    return;
+  }
+  console.log("Database connected");
+  const db = client.db(`${process.env.DB_NAME}`);
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
